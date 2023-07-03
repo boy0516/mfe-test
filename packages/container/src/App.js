f0f6f4be0d4a1ab4b78a2d0efcf532d20d879ae0
boy0516 <boy0516@naver.com>
2023-07-03 19:01:41 +0900

    dash board add

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,8 @@
-import React, {lazy, useState} from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
-import { StylesProvider, createGenerateClassName} from '@material-ui/styles'
+import React, {lazy, useState, useEffect} from 'react';
+import { Router, Route, Switch, Redirect} from 'react-router-dom';
+import { StylesProvider, createGenerateClassName} from '@material-ui/styles';
+import { createBrowserHistory } from 'history';
+
 
 import Progress from './components/Progress';
 import Header from './components/Header';
@@ -8,28 +10,44 @@ import { Suspense } from 'react';
 
 const MarketingApp = lazy(() => import('./components/MarketingApp'));
 const AuthApp = lazy(() => import('./components/AuthApp'));
+const DashBoardLazy = lazy(() => import('./components/DashboardApp'));
+
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 })
 
+const history = createBrowserHistory();
+
 export default () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
     
+    useEffect(() => {
+        if (isSignedIn) {
+            history.push('/dashboard');
+        } else {
+            history.push('/')
+        }
+    }, [isSignedIn])
+
     return (
-        <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
+        <Router history={history}>
+            <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header onSignOut={() => setIsSignedIn(false) } isSignedIn={isSignedIn}/>
                     <Suspense fallback={<Progress/>}>
                         <Route path="/auth">
                             <AuthApp onSignIn={() => setIsSignedIn(true)} />
                         </Route>
+                        <Route path="/dashboard">
+                            {!isSignedIn && <Redirect to="/"/>}
+                            <DashBoardLazy/>
+                        </Route>
                         <Route path="/" component={MarketingApp}/>
                     </Suspense>
 
                 </div>
-            </BrowserRouter>
-        </StylesProvider>
+            </StylesProvider>
+        </Router>
     )
 };
