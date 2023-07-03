b21e47a8b7eed8c24dea2f4a4e0a3480387b6610
boy0516 <boy0516@naver.com>
2023-07-03 19:04:12 +0900

    dash board add

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,10 +1,10 @@
 import {createApp} from 'vue';
 import Dashboard from './components/Dashboard.vue'
 //Mount function to start up the app
-const mount = (el) => {
+
+const mount = (el, { onSignIn, onNavigate, }) => {
     const app = createApp(Dashboard);
     app.mount(el);
-
 };
 
 if ( process.env.NODE_ENV === "development"){
